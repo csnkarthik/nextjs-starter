dc25bb8db49c61b61a3062bac27e8c82886a4efe
csnkarthik <karthik.aspx.cs@gmail.com>
2023-12-02 00:34:57 -0800

    troubleshootingn 4.8

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 export default function SmallCard({ Icon, title, slug }) {
   return (
     <a className="card-small" href={`/project/${slug}`}>
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,26 @@
-import SmallCard from '../components/SmallCard';
-import { projectIcons } from '../components/Icons';
+"use client"
+import { useEffect, useState } from 'react';
 
-import { projects } from '../utils/projectsData';
-import { useEffect } from 'react';
+export default function Home() {
 
-const Home = () => 
-{
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
+      const fetchData = async () => {
+          const response = await fetch('/weatherapi/1.0/weather');
+          const newData = await response.json();
+          setData(newData);
+      };
 
-      try {
-        const response = await fetch(`/api/weatherapi/1.0/weather`);
-        const newData = await response.json();
-        setData(newData);
-      } 
-      catch (error) {
-        console.error('Error fetching data:', error);
-      }
+      fetchData();
+  }, []);
 
-    };
-    fetchData();
-
-  }, [data]);
-
-
-  
-  return (  
+  return (
     <div className="home">
       <h1>What Can I Deploy to Static Apps?</h1>
       <div className="card-grid">
-        {projects.map((project) => {
-          const Icon = projectIcons[project.id];
-          return (
-            <SmallCard
-              key={project.id}
-              Icon={Icon}
-              title={project.name}
-              slug={project.slug}
-            />
-          );
-        })}
-        <p>{response}</p>
+        <p>{data}</p>
       </div>
     </div>
   );
 }
-
-export default Home;
