1c09ea355b4445621f0a75563db6db0e1876984b
csnkarthik <karthik.aspx.cs@gmail.com>
2023-12-01 23:30:00 -0800

    troubleshootingn 4.6

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,29 @@ import { projectIcons } from '../components/Icons';
 
 import { projects } from '../utils/projectsData';
 
-const Home = () => (
-  <div className="home">
-    <h1>What Can I Deploy to Static Apps?</h1>
-    <div className="card-grid">
-      {projects.map((project) => {
-        const Icon = projectIcons[project.id];
-        return (
-          <SmallCard
-            key={project.id}
-            Icon={Icon}
-            title={project.name}
-            slug={project.slug}
-          />
-        );
-      })}
+const Home = () => 
+{
+  const response = await(fetch(`/api/weatherapi/weather`)).json();
+
+  return (  
+    <div className="home">
+      <h1>What Can I Deploy to Static Apps?</h1>
+      <div className="card-grid">
+        {projects.map((project) => {
+          const Icon = projectIcons[project.id];
+          return (
+            <SmallCard
+              key={project.id}
+              Icon={Icon}
+              title={project.name}
+              slug={project.slug}
+            />
+          );
+        })}
+        <p>{response}</p>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Home;
